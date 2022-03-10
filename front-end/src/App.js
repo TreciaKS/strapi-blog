eb451d923cd6b8643ac5febe7b2e58e74dc15872
Trecia Kat <tksekgalo@gmail.com>
2022-03-10 19:01:18 +0200

    Updated Appjs and CSS folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards'
+import NavBar from './components/Navbar'
+import BlogList from './components/blogs/BlogList';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <BlogList />
+      <Cards />
+      
     </div>
   );
 }
 
-export default App;
+export default App
